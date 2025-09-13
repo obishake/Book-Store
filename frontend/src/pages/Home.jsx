1e81727513efe952ea0537768a3212537a60cb7f
Abhishek kumar <abishek2002tk@gmail.com>
2025-09-13 16:44:37 +0530

    Update API endpoint for fetching books

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         setLoading(true);
 
         axios
-            .get("https://book-store-backend-7o21.onrender.com/books")
+            .get("https://book-store-backend-ccvg.onrender.com/books")
             .then((res) => {
                 setBooks(res.data.data);
                 setLoading(false);
