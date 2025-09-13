72cb28d991915a00389f05e1573eb674b06791d2
Abhishek kumar <abishek2002tk@gmail.com>
2025-09-13 16:43:42 +0530

    Update API endpoint for deleting books

--- frontend/src/pages/DeleteBooks.jsx
+++ frontend/src/pages/DeleteBooks.jsx
@@ -14,7 +14,7 @@ const DeleteBooks = () => {
     const handleDeleteBook = () => {
         setLoading(true);
         axios
-            .delete(`https://book-store-backend-7o21.onrender.com/books/${id}`)
+            .delete(`https://book-store-backend-ccvg.onrender.com/books/${id}`)
             .then((responce) => {
                 setLoading(false);
                 enqueueSnackbar("Book Deleted successfully", {
